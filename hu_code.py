91802c94134f8b27ce7094929ec4a10b25e37d08
Nubble07 <80662262+Nubble07@users.noreply.github.com>
2021-03-15 15:55:22 -0700

    Update hu_code.py

--- hu_code.py
+++ hu_code.py
@@ -8,13 +8,18 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 from openpyxl import load_workbook
 
 # this line will print the directory that you should have your excel files in to
 print(os.getcwd())
 
-path_inputs = 'Yuma Data Only - Tamimi.xlsx'
+path_inputs = 'Yuma Data.xlsx'
 sheet_name_setup = 'Sheet1'
-inputs = pd.read_excel('Yuma Data Only - Tamimi.xlsx', 'Sheet1')
-
-print(inputs.iloc[:, [0,7,27]]) # column/row counts starting at 0
+inputs = pd.read_excel('Yuma Data.xlsx', 'Sheet1')
+df = inputs.iloc[:, [0,27,7]]  # column/row counts starting at 0
+df = df.sort_values(by=['Heat Units', 'Coliforms /100 ml'], ascending = True) # organizes data in ascending order
+df = df.dropna() # drops all NaN values
+df = df.reset_index(drop = True) # consolidates data points after NaN stripping
+#df[(np.abs(stats.zscore('Coliforms /100 ml')) < 3)] # attempt at z-score FAILED
+df.to_csv('Yuma.csv', index = False) # saves data to .csv file
