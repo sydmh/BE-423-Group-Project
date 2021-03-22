f19eddd03eed69a62ab6c4706dc9c493cee83c3c
Nubble07 <80662262+Nubble07@users.noreply.github.com>
2021-03-22 13:15:12 -0700

    Add files via upload

--- hu_code.py
+++ hu_code.py
@@ -18,8 +18,25 @@ path_inputs = 'Yuma Data.xlsx'
 sheet_name_setup = 'Sheet1'
 inputs = pd.read_excel('Yuma Data.xlsx', 'Sheet1')
 df = inputs.iloc[:, [0,27,7]]  # column/row counts starting at 0
-df = df.sort_values(by=['Heat Units', 'Coliforms /100 ml'], ascending = True) # organizes data in ascending order
-df = df.dropna() # drops all NaN values
-df = df.reset_index(drop = True) # consolidates data points after NaN stripping
-#df[(np.abs(stats.zscore('Coliforms /100 ml')) < 3)] # attempt at z-score FAILED
-df.to_csv('Yuma.csv', index = False) # saves data to .csv file
+df = df.sort_values(by=['Heat Units', 'Coliforms /100 ml'], ascending = True)
+df = df.dropna()
+df = df.reset_index(drop = True)
+df.to_csv('Yuma.csv', index = False)
+
+plt.hist(df['Coliforms /100 ml'])
+plt.show()
+
+tdf, lmbda = stats.boxcox(df['Coliforms /100 ml'])
+plt.hist(tdf)
+print(lmbda)
+
+'''
+slope, intercept, r_value, p_value, std_err = stats.linregress(h0,C)
+plt.figure(1)
+plt.plot(h0, C, 'bo', C, intercept+slope*C)
+plt.xlabel('h0'); plt.ylabel('C')
+plt.legend(['data', 'regression line'], loc='upper left')
+filestem = 'growth1_%dsteps' % 5
+plt.savefig('%spng' % filestem); plt.savefig('%s.pdf' % filestem)
+print('slope = ', slope, 'intercept = ', intercept)
+'''
